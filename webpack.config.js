af4ebb71751cb9b01d5e68443af13dac63e9ad35
James Gosbell <jamesgosbell+1@gmail.com>
2019-06-02 15:07:20 +1000

    Updated tsconfig with baseUrl for imports, added styled-components webpack config, added favicon and updated linting of scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,46 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    plugins: [
+      new TsconfigPathsPlugin({
+        baseUrl: 'src'
+      })
+    ]
+  },
+  optimization: {
+    runtimeChunk: {
+      name: 'vendor'
+    }
   },
   entry: {
-    app: ["./src/index.tsx"],
-    vendor: ["react", "react-dom", "styled-components"]
+    app: ['./src/index.tsx'],
+    vendor: ['react', 'react-dom', 'styled-components']
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/[name].bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js'
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader",
+        loader: 'awesome-typescript-loader',
         options: {
-          getCustomTransformers: path.join(
-            __dirname,
-            "./webpack.ts-transformers.js"
-          )
+          getCustomTransformers: path.join(__dirname, './webpack.ts-transformers.js')
         }
       }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html"
+      template: './src/index.html',
+      favicon: 'favicon.ico'
     })
   ]
 };
